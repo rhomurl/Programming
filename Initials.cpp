0c9e767d4131f9dd983704290af8877217ad1df7
Rhomuel Macahilig <31006518+rhomuel@users.noreply.github.com>
2019-02-09 01:38:34 +0800

    Create Initials.cpp

---
+++ Initials.cpp
@@ -0,0 +1,22 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+cout << "RRRRRRRRRRRRRRRRR\t\tBBBBBBBBBBBBBBBBBBB\t\tMMMMMMMM        MMMMMMMM\n";
+cout << "RRRRRRRRRRRRRRRRRR\t\t  BBBBBBBBBBBBBBBBBB\t\t  MMM  MMM            MMM  MMM \n";
+cout << "RRR\t\tRRR\t\t  BB          BBB\t\t  MMM   MMM          MMM   MMM \n";
+cout << "RRR\t\tRRR\t\t  BB          BBB\t\t  MMM    MMM        MMM    MMM  \n";
+cout << "RRR\t\tRRR\t\t  BB          BBB\t\t  MMM     MMM      MMM     MMM  \n";
+cout << "RRR\t\tRRR\t\t  BB         BBB\t\t  MMM      MMM    MMM      MMM   \n";
+cout << "RRRRRRRRRRRRRRRRRR\t\t  BBBBBBBBBBBBBBBBBB\t\t  MMM       MMM  MMM       MMM  \n";
+cout << "RRRRRRRRRRRR\t\t\t  BBBBBBBBBBBBBBBBBBB\t\t  MMM        MMMMMM        MMM  \n";
+cout << "RRR\tRRRR\t\t\t  BBBBBBBBBBBBBBBBBB\t\t  MMM                   MMM\n";
+cout << "RRR\t RRRR\t\t\t  BB          BBB\t\t  MMM                   MMM\n";
+cout << "RRR\t  RRRR\t\t\t  BB          BBB\t\t  MMM                   MMM\n";
+cout << "RRR\t   RRRR\t\t\t  BB          BBB\t\t  MMM                   MMM\n";
+cout << "RRR\t    RRRR\t\t  BB         BBB\t\t  MMM                   MMM\n";
+cout << "RRR\t     RRRR\t\t BBBBBBBBBBBBBBBBBBB\t\t  MMM                   MMM\n";
+cout << "RRR\t      RRRR\t\tBBBBBBBBBBBBBBBBBBB\t\tMMMMMMM                    MMMMMMM\n";
+}
